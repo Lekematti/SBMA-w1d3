78234a2b377c7732601485041c41ac45b961e26d
Leo Koskimäki <103430948+Lekematti@users.noreply.github.com>
2024-01-19 16:45:53 +0200

    --

--- app/src/main/java/com/example/sbma_w1d3/MainActivity.kt
+++ app/src/main/java/com/example/sbma_w1d3/MainActivity.kt
@@ -7,27 +7,33 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.R
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Send
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.sbma_w1d3.ui.theme.SBMAw1d3Theme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,10 +41,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             SBMAw1d3Theme {
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier
+                        .padding(5.dp),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    MessageCard(Message("Android", "Jetpack Compose"))
+                    MessageCard(Message("Lexi", "Jetpack Compose"))
                 }
             }
         }
@@ -51,17 +58,30 @@ fun MessageCard(msg: com.example.sbma_w1d3.Message) {
     Card(
         modifier = Modifier
             .fillMaxWidth()
+            .height(300.dp)
             .padding()
     ) {
-        Row(modifier = Modifier.padding(all = 10.dp)) {
+        Column {
+            Image(
+                painter = painterResource(id = R.drawable.patric),
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight(0.6F),
+                contentScale = ContentScale.FillWidth
+            )
+        }
+        Row {
             Image(
                 painter = painterResource(R.drawable.humu),
                 contentDescription = null,
                 modifier = Modifier
+                    .padding(8.dp)
                     .size(45.dp)
                     .clip(CircleShape)
                     .border(1.5.dp, MaterialTheme.colorScheme.primary, CircleShape)
             )
+
             Spacer(modifier = Modifier.width(8.dp))
 
             Column {
@@ -81,6 +101,16 @@ fun MessageCard(msg: com.example.sbma_w1d3.Message) {
                     )
                 }
             }
+        }
+        Button(
+            onClick = { /*TODO*/ },
+            modifier = Modifier
+                .padding(8.dp)
+                .align(Alignment.End)
+        ) {
+            Icon(imageVector = Icons.Default.Send, contentDescription = null)
+            Spacer(modifier = Modifier.width(5.dp))
+            Text(text = "Send")
         }
     }
 }
